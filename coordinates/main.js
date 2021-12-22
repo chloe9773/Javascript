6143915a9d88733f327fe0a1f259b664291fec65
Chloe Park <jeonghwapark@outlook.com>
2021-12-23 07:58:56 +0900

    20211223_002 - 좌표를 찾아라_최종

--- coordinates/main.js
+++ coordinates/main.js
@@ -1,2 +1,24 @@
 'use strict';
 
+const target = document.querySelector('#target');
+const tag = document.querySelector('.tag');
+const horizontal = document.querySelector('.horizontal');
+const vertical = document.querySelector('.vertical');
+const targetMove = window.addEventListener('mousemove', event => {
+    const x = event.clientX;
+    const y = event.clientY;
+      
+    console.log(x, y);
+      
+    target.style.position =  "absolute";
+    target.style.left =  `${x}px`;
+    target.style.top = `${y}px`;
+
+    tag.innerHTML = `${x}px, ${y}`;
+    tag.style.left =  `${x}px`;
+    tag.style.top = `${y}px`;
+
+    horizontal.style.top = `${y}px`;
+    vertical.style.left = `${x}px`;
+});
+
