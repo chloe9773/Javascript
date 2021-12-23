56571224ba2e4c33041e46b81448c920b89b0803
Chloe Park <jeonghwapark@outlook.com>
2021-12-23 10:35:48 +0900

    20211223_004 - 좌표를 찾아라 성능 개선 버전

--- coordinates/main.js
+++ coordinates/main.js
@@ -4,21 +4,32 @@ const target = document.querySelector('#target');
 const tag = document.querySelector('.tag');
 const horizontal = document.querySelector('.horizontal');
 const vertical = document.querySelector('.vertical');
-const targetMove = window.addEventListener('mousemove', event => {
-    const x = event.clientX;
-    const y = event.clientY;
-      
-    console.log(x, y);
-      
-    target.style.position =  "absolute";
-    target.style.left =  `${x}px`;
-    target.style.top = `${y}px`;
 
-    tag.innerHTML = `${x}px, ${y}`;
-    tag.style.left =  `${x}px`;
-    tag.style.top = `${y}px`;
+addEventListener('load', () => {
+    // 성능 개선 추가 수정
+    const targetRect = target.getBoundingClientRect();
+    const targetHalfWidth = targetRect.width / 2;
+    const targetHalfHeight = targetRect.height / 2;
+    const targetMove = window.addEventListener('mousemove', event => {
+        const x = event.clientX;
+        const y = event.clientY;
+        
+        console.log(x, y);
+        
+        // target.style.position =  "absolute";
+        // target.style.left =  `${x}px`;
+        // target.style.top = `${y}px`;
+        target.style.transform = `translate(${x - targetHalfWidth}px, ${y - targetHalfHeight}px)`;
 
-    horizontal.style.top = `${y}px`;
-    vertical.style.left = `${x}px`;
+        tag.innerHTML = `${x}px, ${y}`;
+        tag.style.transform = `translate(${x}px, ${y}px)`;
+        // tag.style.left =  `${x}px`;
+        // tag.style.top = `${y}px`;
+
+        // horizontal.style.top = `${y}px`;
+        // vertical.style.left = `${x}px`;
+        horizontal.style.transform = `translateY(${y}px)`;
+        vertical.style.transform = `translateX(${x}px)`;
+    });
 });
 
